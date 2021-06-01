0ac69ef19bb3093bad28b811013c0a637badd6c3
josejavier <82359964+josejaviersanahuja@users.noreply.github.com>
2021-06-01 05:58:06 +0100

    añadimos mongoose y como usarlo

--- src/pages/Csstutorial/TransitionProperty.js
+++ src/pages/Csstutorial/TransitionProperty.js
@@ -17,7 +17,7 @@ export default function TransitionProperty() {
                         </ul>
                     </div> : null}
             </div>
-            <div className="marco"><h4>Transition Functions</h4>
+            <div className="marco"><h4>Transform Functions</h4>
             <div>Con <b>transform:translate()</b> Podemos pasar 2 valores e intercambiar las posiciones de dos estados diferentes. </div>
                 <a href onClick={() => abrirCerrar(1)} ><h5>EJEMPLOS: </h5></a>
                 {open[1] ?
@@ -33,10 +33,26 @@ export default function TransitionProperty() {
                         </ul>
                     </div> : null}
             </div>
-            <div className="marco"><h4>Animations</h4>
-                <div><b>w/ keyframes</b>  </div>
+            <div className="marco"><h4>Transition Functions</h4>
+            <div>Con <b>transition: property delay duration timing function </b> Podemos pasar valores para causar un efecto visual en las transformaciones. </div>
                 <a href onClick={() => abrirCerrar(2)} ><h5>EJEMPLOS: </h5></a>
                 {open[2] ?
+                    <div> Digamos que hacemos la transición de un botón a través de un hover que modifica el background.
+                        <ul>
+                            <li> <b>hover {"{transform:translate(50px,0)}"}</b> || <button className="boton2"> Background Blue </button> </li>
+                            <li> <b>hover {"{transform:scale(2.5)}"}</b> || aumenta el tamaño de todo en el objeto. Nota: margin, padding, dejan de aplicarse bien </li>
+                            <li> <b>hover {"{transform:rotate(-45deg)}"}</b> || <button className="boton2" style={{transform:'rotate(-45deg)'}}> 45º </button> </li>
+                            <li> <b>hover {"{transform:skewX(-45deg)}"}</b> || <button className="boton2" style={{transform:'skewX(-45deg)'}}> 45º </button> </li>
+                            <li> <b>hover {"{transform:skewY(-45deg)}"}</b> || <button className="boton2" style={{transform:'skewY(-45deg)'}}> 45º </button> </li>
+                            <li> <b>hover {"{transform:matrix(1,0.45,0.45,1, 100, 50)}"}</b> || parametros del matrxi(scale x, skewX, skewY, scale y, translate x, translate y)
+                            <button className="boton2" style={{transform:'matrix(1,0.45,0.45,1, 100, 10)'}}> 45º </button> </li>
+                        </ul>
+                    </div> : null}
+            </div>
+            <div className="marco"><h4>Animations</h4>
+                <div>Con <b>w/ keyframes</b> podemos crear animaciones y con el comando <b>animation: name duration timing-function delay iteration-count direction fill-mode;</b> podemos generar efectos diferentes a cada animación.</div>
+                <a href onClick={() => abrirCerrar(3)} ><h5>EJEMPLOS: </h5></a>
+                {open[3] ?
                     <div className="animacion">
                         <div className="animationcode"> 
                         <p>{"@keyframes animationBackGround {"}</p>
--- src/pages/Fullstack/MongoDB.js
+++ src/pages/Fullstack/MongoDB.js
@@ -22,6 +22,52 @@ export default function MongoDB() {
                     text: "Instalará esta dependencia de producción. Recuerda quitar el caret ^"
                 }
             ]
+        },
+        segundo:{
+            title: "Creamos un modulo con Mongoose y Conectamos",
+            defBreve:"Podremos establecer la conexión a la BD y hacer pruebas para confirmar la conexión y el buen funcionamiento. Para eso creamos el archivo mongo.js.",
+            arrayCodigo:[
+                {
+                    cod:"const mongoose = require('mongoose'); const password = require('./password')",
+                    text: "Requerimos mongoose, recuerdas el password que dijimos era tan importante??? lo copiamos también."
+                },
+                {
+                    // eslint-disable-next-line no-template-curly-in-string
+                    cod:"const connectionString = mongodb+srv: //zitrojj: ${password} @mypokemons. j6lhy.mongodb.net /MyPokemons ?retryWrites= true&w= majority",
+                    text: "El driver de mongoDB también dijimoms que es importante guardar??? bueno, es importante."
+                },
+                {
+                    cod:"mongoose.connect ( connectionString, { useCreateIndex: true, useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true }) .then(() => { console.log ('database connected?') }).catch(e => { console.error(e) })",
+                    text: "Establecemos la conexión con mongoose"
+                }
+            ]
+        },
+        tercero:{
+            title: "Ahora creamos un contrato de objeto dentro de nuetra API",
+            defBreve:"Usaremos 2 módulos de mongoose, Schema y model.",
+            arrayCodigo:[
+                {
+                    cod:"const { Schema, model } = require('mongoose')",
+                    text: "Schema es la declaración del contrato y model es la declaración del objeto que cumple ese contrato. La colección dentro de la BD tendrá el nombre del model. "
+                },
+                {
+                    // eslint-disable-next-line no-template-curly-in-string
+                    cod:"const zitropokemonSchema = new Schema ({ id: Number, name: String, base_attack: Number, base_defense: Number, base_stamina: Number, forms: Array, nodata: Boolean })",
+                    text: "Este modelo lo usé en mi app smartpokemon go."
+                },
+                {
+                    cod:"const Zitropokemon = model('Zitropokemon', zitropokemonSchema)",
+                    text: " Creamos el nombre del objeto, y el contrato que debe cumplir. ya podemos crear un objeto"
+                },
+                {
+                    cod:"const zitropok = new Zitropokemon ({ id: 235, name: 'Smeargle', base_attack: 100, base_defense: 100, base_stamina: 100, forms: [ { form: 'Normal' } ], nodata: false })",
+                    text: " Este objeto no tiene sentido haberlo creado en este módulo, pero servirá de prueba"
+                },
+                {
+                    cod:"zitropok.save() .then(result => { console.log(result) mongoose.connection.close() }).catch(e => { console.error(e) })",
+                    text: "Con este comando guardamos el objeto en la base de datos, en la colección zitropokemons "
+                }
+            ]
         }
     }
 
@@ -31,6 +77,16 @@ export default function MongoDB() {
                 title={detalles.primero.title}
                 defBreve={detalles.primero.defBreve}
                 arrayCodigo={detalles.primero.arrayCodigo}
+            />
+            <DetallesSubtema
+                title={detalles.segundo.title}
+                defBreve={detalles.segundo.defBreve}
+                arrayCodigo={detalles.segundo.arrayCodigo}
+            />
+            <DetallesSubtema
+                title={detalles.tercero.title}
+                defBreve={detalles.tercero.defBreve}
+                arrayCodigo={detalles.tercero.arrayCodigo}
             />
         </div>
     )
