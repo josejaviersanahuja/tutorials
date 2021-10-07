033c5a3edb28a3e6f4e870eb356f451fdc1657e6
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2021-10-07 12:27:45 +0100

    navegación con parámetros

--- src/pages/AndroidJava/Navegation.js
+++ src/pages/AndroidJava/Navegation.js
@@ -27,6 +27,22 @@ startActivity(intentAbout);`,
                     text:"Este es el código que se le puede pasar a un botón que se encuentre en MainActivity y te redirija a la página About de tu App. De esta forma también, podemos volver atrás con el botón principal de atrás de Andoird. Esto significa que es similar a un navigation.push."
                 }
             ]
+        }, tercero:{
+            title:"Navegación con un parámetro",
+            defBreve:"Ahora vamos a ver cómo navegar pasando algún parámetro a la siguiente actividad. Vamos a ello.",
+            arrayCodigo:[
+                {
+                    cod:`// Este código está dentro de un RecyclerView, por eso el context en vez de el this
+Intent intent = new Intent(context, BookActivity.class);
+intent.putExtra("position", position);
+context.startActivity(intent);`,
+                    text:"Con el intent.putExtra podemos pasar una clave key-value que va a poder ser leída en la actividad a la que nos dirigimos. En este caso se trata de un entero con la posición del elemento dentro del arrayList. Veamos cómo acceder a dicha clave en nuestro BookActivity.class."
+                },{
+                    cod:` Intent intent = getIntent();
+int position = intent.getIntExtra("position", 0);`,
+                    text:"Definimos un intent pero no uno nuevo, sino el que nos trajo hasta esta actividad con getIntent. Luego extraemos el valor con getIntExtra y pasamos la clave, pero también un valor por defecto (Por si acaso no funciona). De esa manera, podemos rebuscar el libro dentro de nuestro arrayList en la posición position y podemos extraer los datos de ahí."
+                }
+            ]
         }
     }
 
@@ -46,6 +62,11 @@ startActivity(intentAbout);`,
                 title={detalles.segundo.title}
                 defBreve={detalles.segundo.defBreve}
                 arrayCodigo={detalles.segundo.arrayCodigo}
+            />
+            <DetallesSubtema
+                title={detalles.tercero.title}
+                defBreve={detalles.tercero.defBreve}
+                arrayCodigo={detalles.tercero.arrayCodigo}
             />
         </div>
     )
