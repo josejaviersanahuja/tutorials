6d97833af1107de598f264d2bd4c46b11827388b
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2021-10-30 13:32:54 +0100

    callback en java

--- src/pages/AndroidJava/Fragments.js
+++ src/pages/AndroidJava/Fragments.js
@@ -119,6 +119,131 @@ fragmentTransaction.commit();`,
                     text:"Dentro del ciclo de vida del fragment existe el método onAttach, ahí es donde podemos obtener los datos del bundle que hemos pasado."
                 }
             ]
+        }, tercero:{
+            title:"Pasando datos del Fragment al MainActivity.",
+            defBreve:"Muchas veces los Fragments crean interacciones con el usuario y dichas interacciones deben ser conocidas fuera del Fragment en el MainActivity o la actividad que contenga el Fragment. Para eso hay que saber como pasar datos del Fragment a la actividad que lo contenga. La respuesta es CALLBACKS. Aprenderemos cómo se pasan callbacks en JAVA. Vamos a ello.",
+            arrayCodigo:[
+                {
+                    cod:`package com.zitrojjdev.logssaveinstanceandfragments.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.zitrojjdev.logssaveinstanceandfragments.R;
+
+import java.util.ArrayList;
+
+public class FirstFragment extends Fragment {
+
+    private static final String TAG = "FirstFragment";
+
+    private Spinner countrySpinner;
+
+// 1. this is the callback definition
+    public interface GetCountry {
+            void onGetCountryResult(String country);
+    }
+// 2. And here we create the callback
+    private GetCountry getCountry;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: zitrojj start");
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        // more code
+        countrySpinner = view.findViewById(R.id.countriesSpinner);
+        final ArrayList<String> countries = new ArrayList<>();
+        countries.add("Germany");
+        countries.add("Spain");
+        countries.add("USA");
+        countries.add("England");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, countries);
+
+        countrySpinner.setAdapter(adapter);
+
+// 3. here we initialize it, because it is coming from the mainActivity
+        try {
+            getCountry = (GetCountry) getActivity();
+        } catch (ClassCastException e){
+            e.printStackTrace();
+            Log.d(TAG, "onCreateView: zitrojj a problem with the callback onGetCountryResult");
+        }
+
+        countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+// 4. Ponemos exactamente el lugar donde queremos ejecutar nuestro callback
+                getCountry.onGetCountryResult(countries.get(i)); // pasamos el country
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        Bundle bundle = getArguments();
+        if (bundle != null){
+            Log.d(TAG, "onAttach: zitrojj start " + bundle.getString("name"));
+
+        }
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        Log.d(TAG, "onDetach: zitrojj start");
+        super.onDetach();
+    }
+}
+`,
+                    text:"En 4 pasos podemos definir la interface, crear nuestra variable interface, inicializarla dentro de un try/catch y ejecutarla en el lugar del Fragment que más nos interese. Ahora veamos cómo se define en nuestro MainActivity."
+                },{
+                    cod:`package com.zitrojjdev.logssaveinstanceandfragments;
+
+        // ... IMPORTS
+
+// 1. implementamos la interface que creamos
+
+import com.zitrojjdev.logssaveinstanceandfragments.Fragments.FirstFragment;
+public class MainActivity extends AppCompatActivity implements FirstFragment.GetCountry {
+
+    private static final String TAG = "MainActivity";
+    ...
+    
+    // 2. Definir el callback dentro del activity. (abrv. ctrl +i)
+    @Override
+    public void onGetCountryResult(String country) {
+
+        // lógica que deseamos ejecutar en el mainactivity. El country vendrá definido en el callback
+
+        Log.d(TAG, "onGetCountryResult: zitrojj entró al callback");
+        Toast.makeText(this, "the country selected is "+country, Toast.LENGTH_LONG).show();
+        finalText = "Country selected is " + country;
+        hasBeenClicked =true;
+        textView.setText(finalText);
+    }`,
+                    text:"Y en 2 pasos definimos nuestro callback dentro de nuestra Actividad."
+                }
+            ]
         }
     }
 
@@ -138,6 +263,11 @@ fragmentTransaction.commit();`,
                 title={detalles.segundo.title}
                 defBreve={detalles.segundo.defBreve}
                 arrayCodigo={detalles.segundo.arrayCodigo}
+            />
+            <DetallesSubtema
+                title={detalles.tercero.title}
+                defBreve={detalles.tercero.defBreve}
+                arrayCodigo={detalles.tercero.arrayCodigo}
             />
         </div>
     )
