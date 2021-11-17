f0617ea6f0eff7f0ff9c800c7d260134f7d7c62f
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2021-11-17 23:44:46 +0000

    regex terminado

--- src/pages/Regex/AllRegex.js
+++ src/pages/Regex/AllRegex.js
@@ -23,15 +23,100 @@ export default function AllRegex() {
                     text: "Dejo un link donde poder jugar con las regex, sirve de playground para comprobar ciertas funcionalidades de las regex. El gran verdadero poder de regex aparece cuando trabajamos con los selectores especiales +, ^, [], ? . Vamos a ello."
                 },{
                     cod:`// selector +
-/e+/g   // esto va a encontrar todas las e, pero si hay varias ee juntas, las machea en conjunto
+/e+/g   // matches as many e's as you can, pero si hay varias ee juntas, las machea en conjunto
             // por ejemplo hacemos "tree".replace("/e+/, a") 
             // el resultado no va a ser traa, 
             // el resultado será tra
 
+// selector ?, optional selector
+/ea?/g // machea todas las e's solas, y las ea
 
+// selector *
+/ea*/g // es como una combinación del + y el ?
+        // ya que machea desde 0 a's hasta todas las a's que hayan después de la e
+        // ejemplo heeaaaaaaa, matchea una e, y también la eaaaaaaa
+
+// selector .
+/.ea/g // matchea cualquier character que haya, EXCEPTO una nueva línea.
+        // ejemplo 'brea lea' matchea rea y lea.
+
+
+// selector \, sirve para seleccionar characteres que son selectores
+/\./g // Esto machea todos los puntos en el texto.
+
+// selector \d, digits
+/\d/g // selecciona todos los dígitos del texto
+
+// selector \w \W, selecciona palabras
+/\w/g // esto matchea todas las letras de las palabras sin signos de puntuación.
+/\W/g // esto matchea todo lo que no son letras
+        // signos de puntuación y espacios en blanco
+
+// selector \s \S, selecciona todos los spaces
+/\s/g // todos los espacios en blanco son seleccionados.
+/\S/g // todas las letras y puntuaciones son seleccionados.
+
+// Advanced selector \w{4,5}
+/\w{4}/g // machea palabras con 4 letras seguidas 
+            // ejemplo 'esto es unaprueba' 
+            // machea esto unap rueb
+
+/\w{4,}/g // machea palabras de 4 letras o más
+            // machea esto unaprueba
+
+/\w{4,5}/g // machea palabras de 4 o 5 letras
+            // machea esto unapr ueba
+
+// selector []
+/[ab]ed/g // machea aed y bed en el texto
+/[a-z]ed/g // machea todas las opciones que empiecen con minuscula y terminen en ed
+            // ejemplo 'pedestal medalla Cede'
+            // machea ped med
+
+// selector (), groups
+/(t|T)he/g // Esto va a matchear the y The 
+/(t|T){4,}he/g // esto va a machear 4 o más t ó T seguidas 
+
+// selector ^
+/^T/g // machea solo si la primera letra del párrafo es T
+/^I/gm // el flag m significa multiline
+        // suponiendo que una segunda o tercera línea empiece con I, esa será macheada
+
+// selector $
+/$z/g // machea solo el final del texto
+/$z/gm // machea solo el el final de la línea si termina en z
 
 `,
-                    text:""
+                    text:"Esto representa el 90 % de los selectores que usaremos en los regex. Pero vamos a estudiar también algunos selectores más complejos también."
+                },{
+                    cod:`// LookBehind
+/(?<=[tT]he)./g // ejemplo 'the theater'
+                // machea ' ' y a
+                // básicamente decimos machea todo lo que esté precedido por the o The
+                // o todo lo que venga después de the o The
+
+/(?<![tT]he)./g // machea todos los chars que no tenga the o The antes
+
+// LookForward
+/.(?=at)/g // machea todo char que esté antes de at
+/.(?!at)/g // machea todo char que no esté antes de at
+
+// small example
+/(\d{3})[ -]?(\d{3})[ -]?(\d{4})/g
+
+// esto va a agrupar o machear las siguientes 3 lineas de igual forma
+// 1234567890
+// 123-456-7890
+// 123 456 7890
+// Si además hacemos replace
+$1$2$3 // esto va a juntar los 3 grupos
+
+// demosle nombres a los grupos
+/(?<tagName1>\d{3})[ -]?(\d{3})[ -]?(\d{4})/g
+
+$tagName1$2$3 // así quedaría el replace
+`,
+                    text:"Ahora ya podemos entender bien las regex. Solo queda ver formas ingeniosas de cómo usarlas."
                 }
             ],
             url:"https://regexr.com"
