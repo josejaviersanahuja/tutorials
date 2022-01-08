28b12a59e913be07ea2b6ca0494d8028c4ba8bd1
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2022-01-08 04:15:42 +0000

    Hemos comenzado a hacer queries

--- src/pages/Postgres/PostgresCommands.js
+++ src/pages/Postgres/PostgresCommands.js
@@ -129,6 +129,192 @@ SELECT last_name, date_of_birth, email FROM person;
                 }
             ],
             url:"https://www.mockaroo.com/"
+        }, quinto:{
+            title:"Empezamos a usar QUERIES",
+            defBreve:"Aprender todas las queries relacionadas a SORTING y FILTER es mucho más potente y eficiente que pedir la información y luego mutar los datos. Aquí es donde todo empieza a ponerse interesante..",
+            arrayCodigo:[
+                {
+                    cod:`SELECT * FROM car ORDER BY make;
+// Por defecto lo ordena de forma ascendente.
+
+# Ahora veamos la forma explicita.
+
+learningtest=# SELECT * FROM car ORDER BY make DESC;
+learningtest=# SELECT * FROM car ORDER BY make ASC;
+
+# Ahora veamos con doble parámetro.
+
+SELECT * FROM car ORDER BY make, model ASC;
+// Este va a ordenar primero por make, y luego por modelo.
+
+// id  |     make      |        model         |  price   
+// ------+---------------+----------------------+----------
+//   445 | Acura         | CL                   | 58360.85
+//   423 | Acura         | Integra              | 26535.16
+//   412 | Acura         | Integra              | 19412.79
+//   114 | Acura         | Legend               | 50616.55
+//   538 | Acura         | Legend               | 71187.65
+//   616 | Acura         | Legend               | 23421.98
+//   438 | Acura         | RDX                  | 74393.51
+//   786 | Acura         | RL                   | 96683.15
+//   856 | Acura         | RL                   | 64601.54
+//   280 | Acura         | RL                   | 17143.90
+//   981 | Acura         | RL                   | 35911.23
+//   480 | Acura         | SLX                  | 14628.25
+//   499 | Acura         | TL                   | 20839.70
+//   992 | Acura         | TSX                  | 24483.86
+//   852 | Acura         | TSX                  | 27400.29
+//   839 | Acura         | TSX                  | 89258.32
+//   953 | Alfa Romeo    | 164                  | 77644.93
+//   864 | Aston Martin  | DB9                  | 54816.06
+//   752 | Aston Martin  | DB9 Volante          | 58246.71
+//   418 | Aston Martin  | DB9 Volante          | 44264.25
+//    36 | Aston Martin  | DB9 Volante          | 23156.38
+//   832 | Aston Martin  | Rapide               | 29342.45
+//   470 | Aston Martin  | V8 Vantage S         | 98230.44
+//   980 | Aston Martin  | Virage               | 22690.09
+//   286 | Audi          | 100                  | 19952.57
+//   766 | Audi          | 200                  | 79318.40
+//   314 | Audi          | 200                  | 24604.27
+//   218 | Audi          | 200                  | 74560.17
+//   712 | Audi          | 200                  | 36214.38
+
+`,
+                    text:"Aquí hemos hecho un SORTING. Ahora lo siguiente que hagamos será sacar una lista de todas las marcas de coche."
+                },{
+                    cod:`SELECT make FROM car;
+SELECT make FROM car ORDER BY make;
+
+// Ambos queries me dará un listado de marcas repetidas, una ordenada y la otra no. 
+
+# Lo que queremos es un listado de marcas no repetidas.
+
+SELECT DISTINCT make FROM car ORDER BY make;
+
+// make      
+// ---------------
+//  Acura
+//  Alfa Romeo
+//  Aston Martin
+//  Audi
+//  Bentley
+//  BMW
+//  Buick
+//  Cadillac
+//  Chevrolet
+//  Chrysler
+//  Citroën
+//  Daewoo
+//  Daihatsu
+//  Dodge
+//  Eagle
+//  Ferrari
+//  Ford
+//  Geo
+//  GMC
+//  Honda
+//  Hummer
+//  Hyundai
+//  Infiniti
+//  Isuzu
+//  Jaguar
+//  Jeep
+//  Jensen
+//  Kia
+//  Lamborghini
+
+`,
+                    text:"Al usar la palabra DISTINCT evitamos datos repetidos."
+                }
+            ]
+        }, sexto:{
+            title:"Filtrado WHERE",
+            defBreve:"Seguimos con más queries. Esta vez para filtrar usaremos el WHERE",
+            arrayCodigo:[
+                {
+                    cod:`SELECT * FROM car WHERE make = 'Ferrari';
+// id  |  make   |     model      |  price   
+// -----+---------+----------------+----------
+//     229 | Ferrari | 612 Scaglietti | 21302.73
+//     568 | Ferrari | 612 Scaglietti | 99022.72
+// (2 filas)`,
+                    text:"Todo parece claro. Vamos a complicarlo un poco más."
+                },{
+                    cod:`SELECT * FROM car WHERE make = 'Ferrari' OR make = 'Audi';
+// id  |  make   |     model      |  price   
+// -----+---------+----------------+----------
+//     8 | Audi    | S6             | 62251.73
+//     88 | Audi    | A6             | 92814.88
+//     98 | Audi    | RS 4           | 10076.23
+// 168 | Audi    | 90             | 49186.57
+// 218 | Audi    | 200            | 74560.17
+// 229 | Ferrari | 612 Scaglietti | 21302.73
+// 286 | Audi    | 100            | 19952.57
+// 309 | Audi    | 90             | 90989.87
+// 314 | Audi    | 200            | 24604.27
+// 342 | Audi    | Cabriolet      | 18065.58
+// 344 | Audi    | V8             | 64417.33
+// 395 | Audi    | A4             | 72995.18
+// 498 | Audi    | 5000S          | 30666.07
+// 521 | Audi    | S8             | 20484.41
+// 568 | Ferrari | 612 Scaglietti | 99022.72
+// 615 | Audi    | A4             | 58999.16
+// 631 | Audi    | S6             | 11071.18
+// 642 | Audi    | A8             | 59978.34
+// 712 | Audi    | 200            | 36214.38
+// 715 | Audi    | S6             | 27675.54
+// 761 | Audi    | 200            | 13095.28
+// 766 | Audi    | 200            | 79318.40
+// 943 | Audi    | 4000           | 53207.94
+// 961 | Audi    | S4             | 41859.36
+// 986 | Audi    | A3             | 78108.61
+
+// También podemos ordenarlo.
+SELECT * FROM car WHERE make = 'Ferrari' OR make = 'Audi' ORDER BY make;
+
+`,
+                    text:"Podemos unir queries más complejas con los operadores lógicos AND y OR."
+                }
+            ]
+        }, septimo:{
+            title:"Limit, Offset y Fetch.",
+            defBreve:"Ya hemos visto la importancia de conseguir controlar el número de parámetros de datos que mandamos, paginación y demás. Vamos a aprender a crear queries que hagan eso por nosotros.",
+            arrayCodigo:[
+                {
+                    cod:`SELECT * FROM car LIMIT 5;
+// id |    make    |    model     |  price   
+// ----+------------+--------------+----------
+//     1 | Land Rover | Sterling     | 87665.38
+//     2 | GMC        | Acadia       | 17662.69
+//     3 | Ford       | F250         | 16819.19
+//     4 | Chevrolet  | Sportvan G30 | 31932.31
+//     5 | Lexus      | ES           | 92820.92
+// (5 filas)
+                   
+SELECT * FROM car OFFSET 5 LIMIT 5;
+//  id |   make    |    model     |  price   
+// ----+-----------+--------------+----------
+//   6 | Kia       | Sorento      | 41368.24
+//   7 | Toyota    | Sienna       | 22861.43
+//   8 | Audi      | S6           | 62251.73
+//   9 | Subaru    | Justy        | 92998.82
+//  10 | Chevrolet | Express 2500 | 10823.69
+// (5 filas)
+
+LIMIT no es un comando típico de SQL. FETCH si es el comando adecuado.
+
+SELECT * FROM car OFFSET 5 FETCH FIRST 3 ROW ONLY;
+//  id |  make  |  model  |  price   
+// ----+--------+---------+----------
+//   6 | Kia    | Sorento | 41368.24
+//   7 | Toyota | Sienna  | 22861.43
+//   8 | Audi   | S6      | 62251.73
+// (3 filas)
+
+`,
+                    text:"Y ahora ya sabemos cómo vamos a poder crear queries para crear paginación de datos."
+                }
+            ]
         }
     }
 
@@ -163,6 +349,18 @@ SELECT last_name, date_of_birth, email FROM person;
                 defBreve={detalles.cuarto.defBreve}
                 arrayCodigo={detalles.cuarto.arrayCodigo}
                 url={detalles.cuarto.url}
+                language='sql'
+            /> 
+            <DetallesSubtema
+                title={detalles.quinto.title}
+                defBreve={detalles.quinto.defBreve}
+                arrayCodigo={detalles.quinto.arrayCodigo}
+                language='sql'
+            /> 
+            <DetallesSubtema
+                title={detalles.sexto.title}
+                defBreve={detalles.sexto.defBreve}
+                arrayCodigo={detalles.sexto.arrayCodigo}
                 language='sql'
             /> 
         </div>
